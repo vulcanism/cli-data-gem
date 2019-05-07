6ac5e70322a63681db464ea8e00b4843bb87f282
Savannah C <cynomania@gmail.com>
2019-05-07 16:48:32 -0400

    companions class created, more notes

--- cli_data_gem/lib/cli_data_gem/companions.rb
+++ cli_data_gem/lib/cli_data_gem/companions.rb
@@ -1,1 +1,5 @@
 # Class to create companion objects, which the cli will iterate through
+
+class Companions
+
+end
---
+++ cli_data_gem/lib/cli_data_gem/scraper.rb
@@ -0,0 +1,4 @@
+# Scrapes data from url: https://dragonage.fandom.com/wiki/Companions_(Origins)
+
+require 'open-uri'
+require 'nokogiri'
