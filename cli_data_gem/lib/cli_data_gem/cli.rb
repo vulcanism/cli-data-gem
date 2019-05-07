cc7f994e9431594855ddb44309f4c49cdd785f44
Savannah C <cynomania@gmail.com>
2019-05-07 16:44:23 -0400

    more notes, more cli development. all working

--- cli_data_gem/lib/cli_data_gem/cli.rb
+++ cli_data_gem/lib/cli_data_gem/cli.rb
@@ -3,6 +3,7 @@ class CliDataGem::CLI
   def start
     list
     options
+    goodbye
   end
 
   def list
@@ -14,15 +15,20 @@ class CliDataGem::CLI
   def options
     input = ""
     while input != "exit"
-      puts "Which character would you like to know more about?"
+      puts "Which character would you like to know more about? Or type 'list' to see the list again"
       input = gets.strip.downcase
       case input
       when "1"
         puts "More info on 1..."
+      when "list"
+        list
+      else
+        puts "Not a valid entry, please try again"
       end
     end     
   end
 
+  # Exits program, tells user goodbye
   def goodbye
     puts "Exiting"
   end
