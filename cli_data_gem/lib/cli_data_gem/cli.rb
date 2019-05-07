2fabc0d2196ef1eaeeb9694b7b5be13540677324
Savannah C <cynomania@gmail.com>
2019-05-07 12:41:56 -0400

    expanded cli class, added scraper

--- cli_data_gem/lib/cli_data_gem/cli.rb
+++ cli_data_gem/lib/cli_data_gem/cli.rb
@@ -1,7 +1,17 @@
 class CliDataGem::CLI
   
   def start
-    puts "Testing"
+    list
+    options
+  end
+
+  def list
+    puts "Available Companions:"
+    # Lists companions using scraper.rb
+  end
+  
+  def options
+    # Asks which character user would like more info on, or if they'd like to exit
   end
   
 end
